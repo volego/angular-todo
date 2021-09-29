afd3160b9f8b54f713fe1c0e24d9656648d26266
volego <oleg.v@masons.mobi>
2021-09-29 16:00:40 +0300

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+
+    constructor(
+        private translateService: TranslateService
+    ) {
+        // устанавливаем язык по умолчанию
+        this.translateService.setDefaultLang(localStorage.getItem('language')??'ru');
+    }
+
+    setLanguage(languageCode: string) {
+        // устанавливаем выбранный язык
+        this.translateService.use(languageCode);
+
+        localStorage.setItem('language',languageCode);
+        // для примера переводим строку вне шаблона, используя
+        // для этого TranslateService
+        //this.translateService.get('info.about').pipe(first()).subscribe((value) => console.log(value));
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
-import { AppComponent } from './app.component';
+// данная функция необходима при AOT компиляции
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
